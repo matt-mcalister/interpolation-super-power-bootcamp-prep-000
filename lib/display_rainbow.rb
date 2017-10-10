4dbe373e0fedfc7add04f43d41920b0251fbe787
Matt McAlister <matt.mcalister93@gmail.com>
2017-10-10 20:11:04 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,11 @@
-# Write your #display_rainbow method here
+def display_rainbow(array)
+  result = []
+  array.each do |color|
+    string = "#{color.chars[0].upcase}: #{color}"
+    if color != array[-1]
+      string = string + ", "
+    end
+    result << string
+  end
+  print result.join
+end
