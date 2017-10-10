a34689f197d67f08283eaef4d328b7d22ffdced3
Matt McAlister <matt.mcalister93@gmail.com>
2017-10-10 20:28:44 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -13,10 +13,12 @@
 
 def display_rainbow(array)
   print "#{array[0][0].upcase}: #{array[0]}, "
-  print "#{array[1][1].upcase}: #{array[1]}, "
-  print "#{array[2][2].upcase}: #{array[2]}, "
-  print "#{array[3][3].upcase}: #{array[3]}, "
-  print "#{array[4][4].upcase}: #{array[4]}, #{array[5][5].upcase}: #{array[5]}, #{array[6][6].upcase}: #{array[6]}, "
+  print "#{array[1][0].upcase}: #{array[1]}, "
+  print "#{array[2][0].upcase}: #{array[2]}, "
+  print "#{array[3][0].upcase}: #{array[3]}, "
+  print "#{array[4][0].upcase}: #{array[4]}, "
+  print "#{array[5][0].upcase}: #{array[5]}, "
+  print "#{array[6][0].upcase}: #{array[6]}, "
 end
 
 colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
