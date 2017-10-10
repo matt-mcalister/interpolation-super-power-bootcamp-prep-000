7112d7ae36c7c9a622b4d2f8aab336d2c2609f1d
Matt McAlister <matt.mcalister93@gmail.com>
2017-10-10 20:12:55 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -7,7 +7,7 @@ def display_rainbow(array)
     end
     result << string
   end
-  puts result.join
+  result.join
 end
 
 display_rainbow(['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
