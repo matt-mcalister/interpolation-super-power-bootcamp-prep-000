828f27eebba00ef61c51883556453fa74f106232
Matt McAlister <matt.mcalister93@gmail.com>
2017-10-10 20:24:53 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,16 +1,20 @@
+# def display_rainbow(array)
+#   result = []
+#   array.each do |color|
+#     string = "#{color.chars[0].upcase}: #{color}"
+#     if color != array[-1]
+#       string = string + ", "
+#     end
+#     result << string
+#   end
+#   result = result.join.to_s
+#   puts result
+# end
+
 def display_rainbow(array)
-  result = []
-  array.each do |color|
-    string = "#{color.chars[0].upcase}: #{color}"
-    if color != array[-1]
-      string = string + ", "
-    end
-    result << string
-  end
-  result = result.join.to_s
-  puts result
+  print "#{array[0][0].upcase}: #{array[0]}, #{array[1][1].upcase}: #{array[1]}, #{array[2][2].upcase}: #{array[2]}, #{array[3][3].upcase}: #{array[3]}, #{array[4][4].upcase}: #{array[4]}, #{array[5][5].upcase}: #{array[5]}, #{array[6][6].upcase}: #{array[6]}, "
 end
 
-# colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
-#
-# display_rainbow(colors)
+colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+
+display_rainbow(colors)
